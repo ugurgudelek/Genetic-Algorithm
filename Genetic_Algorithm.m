1adcdf8cd10ec9f7d2e9f58d0f0165b43e6e5e84
Uğur Güdelek <e174102@metu.edu.tr>
2017-03-14 13:32:37 +0200

    Add files via upload

---
+++ Genetic_Algorithm.m
@@ -0,0 +1,215 @@
+classdef Genetic_Algorithm < handle
+    properties
+        chromosome_len;
+        population_size;
+        crossover_ratio;
+        mutation_ratio;
+        elitism_ratio;
+        chromosome_split;
+        iteration_size;
+        
+        population;
+        fitness_history;
+    end
+    methods(Static)
+        function fitness = fitness_func( chromosome )
+            
+            
+            %   hartman-6-dim-func
+            %   f[0.20169,0.150011,0.476874,0.275332,0.311652,0.6573] =  3.0425
+            %
+            xx = chromosome;
+            alpha = [1.0, 1.2, 3.0, 3.2]';
+            A = [10, 3, 17, 3.5, 1.7, 8;
+                0.05, 10, 17, 0.1, 8, 14;
+                3, 3.5, 1.7, 10, 17, 8;
+                17, 8, 0.05, 10, 0.1, 14];
+            P = 10^(-4) * [1312, 1696, 5569, 124, 8283, 5886;
+                2329, 4135, 8307, 3736, 1004, 9991;
+                2348, 1451, 3522, 2883, 3047, 6650;
+                4047, 8828, 8732, 5743, 1091, 381];
+            
+            outer = 0;
+            for ii = 1:4
+                inner = 0;
+                for jj = 1:6
+                    xj = xx(jj);
+                    Aij = A(ii, jj);
+                    Pij = P(ii, jj);
+                    inner = inner + Aij*(xj-Pij)^2;
+                end
+                new = alpha(ii) * exp(-inner);
+                outer = outer + new;
+            end
+            
+            y = -(2.58 + outer) / 1.94;
+            
+            fitness = -y;
+            
+            
+            
+            
+            
+            %             fitness = -((x-4).^2 + (y+3).^2);
+            %             fitness = -(0.26*(x.^2 + y.^2)- 0.48*x.*y + 32);
+            %             fitness = -(-cos(x)*cos(y)*exp(-((x-pi)^2 + (y-pi)^2)));
+            
+            %
+            %             fact1a = (x1 + x2 + 1)^2;
+            %             fact1b = 19 - 14*x1 + 3*x1^2 - 14*x2 + 6*x1*x2 + 3*x2^2;
+            %             fact1 = 1 + fact1a*fact1b;
+            %
+            %             fact2a = (2*x1 - 3*x2)^2;
+            %             fact2b = 18 - 32*x1 + 12*x1^2 + 48*x2 - 36*x1*x2 + 27*x2^2;
+            %             fact2 = 30 + fact2a*fact2b;
+            %
+            %             fitness = - fact1*fact2;
+        end
+        
+        function [selection,roulette_arr] = roulette( roulette_arr )
+            selection = 1;
+            if ~isempty(roulette_arr)
+                max_of = max(roulette_arr);
+                min_of = min(roulette_arr);
+                roulette_arr = (roulette_arr - min_of)/((max_of - min_of));
+                rand_pnt = rand;
+                for i = 1:size(roulette_arr,1)
+                    if rand_pnt <= roulette_arr(i)
+                        %           i is my selected point
+                        selection = i;
+                        return
+                    end
+                end
+            end
+        end
+        
+        
+    end
+    
+    methods
+        function obj = Genetic_Algorithm(chromosome_len,population_size,crossover_ratio,mutation_ratio,elitism_ratio,chromosome_split,iteration_size)
+            obj.chromosome_len   = chromosome_len;
+            obj.population_size  = population_size;
+            obj.crossover_ratio  = crossover_ratio;
+            obj.mutation_ratio   = mutation_ratio;
+            obj.elitism_ratio    = elitism_ratio;
+            obj.chromosome_split = chromosome_len * chromosome_split;
+            obj.iteration_size   = iteration_size;
+            
+            obj.population = struct('chromosomes', zeros(population_size,chromosome_len), 'fitnesses', zeros(population_size,1));
+            obj.create_population();
+        end
+        
+        
+        function [] = scatter(obj)
+            chroms = obj.population.chromosomes;
+            fits   = obj.population.fitnesses;
+            scatter3(chroms(:,1),chroms(:,2),fits,'.r');
+            %             scatter3(chromosome(1,1),chromosome(1,2),fitness);
+            %             for i = 1:obj.population_size
+            %                 chromosome = obj.population.chromosomes(i,:);
+            %                 fitness = obj.population.fitnesses(i);
+            %                 scatter3(chromosome(1,1),chromosome(1,2),fitness);
+            %             end
+        end
+        
+        function run(obj)
+            obj.calculate_fitnesses()
+
+            for iter = 1:obj.iteration_size
+                obj.elitism()
+                obj.crossover()
+                obj.mutation()
+                obj.calculate_fitnesses()
+                obj.sort_by_field()
+                obj.fitness_history(iter) = obj.population.fitnesses(1);
+            end
+        end
+        
+        function [] = create_population(obj)
+            % create first generation
+            for i = 1:obj.population_size
+                obj.population.chromosomes(i,:) = rand(obj.chromosome_len,1)';
+            end
+        end
+        
+        function [] = calculate_fitnesses(obj)
+            %   calculate fitnesses
+            for i = 1:obj.population_size
+                obj.population.fitnesses(i) = Genetic_Algorithm.fitness_func(obj.population.chromosomes(i,:));
+            end
+        end
+        
+        function [] = sort_by_field(obj)
+            % sort population struct
+            [~, ind]=sort(cell2mat({obj.population.fitnesses}),'descend');
+            obj.population.chromosomes=obj.population.chromosomes(ind,:);
+            obj.population.fitnesses=obj.population.fitnesses(ind);
+        end
+        
+        function [] = elitism(obj)
+            %   elitism
+            obj.sort_by_field()
+            obj.population.chromosomes = obj.population.chromosomes(1:size(obj.population.chromosomes,1)/2,:);
+            obj.population.fitnesses = obj.population.fitnesses(1:size(obj.population.fitnesses,1)/2);
+        end
+        
+        function [] = crossover(obj)
+            %   crossover with roulette
+            summ = sum(obj.population.fitnesses);
+            roulette_arr = zeros(size(obj.population.fitnesses,1),1);
+            for i = 1:size(obj.population.fitnesses,1)
+                roulette_arr(i) = (sum(obj.population.fitnesses(1:i-1)) + obj.population.fitnesses(i) ) / summ;
+            end
+            
+            next_gen = struct('chromosomes', zeros(obj.population_size,obj.chromosome_len), 'fitnesses', zeros(obj.population_size,1));
+            
+            mem_cnt = size(obj.population.fitnesses,1) + 1;
+            
+            next_gen.chromosomes(1:mem_cnt-1,:) = obj.population.chromosomes;
+            next_gen.fitnesses(1:mem_cnt-1,:) = obj.population.fitnesses;
+            while true
+                if rand > (1-obj.crossover_ratio)
+                    if  size(roulette_arr,1) > 1
+                        [idx_1,roulette_arr] = Genetic_Algorithm.roulette(roulette_arr);
+                        roulette_arr = cat(1,roulette_arr(1:idx_1-1),roulette_arr(idx_1+1:size(roulette_arr,1)));
+                        [idx_2,roulette_arr] = Genetic_Algorithm.roulette(roulette_arr);
+                        roulette_arr = cat(1,roulette_arr(1:idx_2-1),roulette_arr(idx_2+1:size(roulette_arr,1)));
+                        
+                        chromosome_1 = obj.population.chromosomes(idx_1,:);
+                        chromosome_2 = obj.population.chromosomes(idx_2,:);
+                        
+                        new_chromosome_1 = chromosome_1;
+                        new_chromosome_2 = chromosome_2;
+                        new_chromosome_1(obj.chromosome_split+1:obj.chromosome_len) = chromosome_2(obj.chromosome_split+1:obj.chromosome_len);
+                        new_chromosome_2(obj.chromosome_split+1:obj.chromosome_len) = chromosome_1(obj.chromosome_split+1:obj.chromosome_len);
+                        
+                        next_gen.chromosomes(mem_cnt,:) = new_chromosome_1;
+                        next_gen.chromosomes(mem_cnt+1,:) = new_chromosome_2;
+                        mem_cnt = mem_cnt + 2;
+                        
+                        if isempty(roulette_arr)
+                            break
+                        end
+                    end
+                end
+            end
+            
+            obj.population = next_gen;
+        end
+        
+        function [] = mutation(obj)
+            %   mutation
+            for i = 1:obj.population_size
+                for j = 1:obj.chromosome_len
+                    if rand > (1-obj.mutation_ratio)
+                        obj.population.chromosomes(i,j) = rand;
+                    end
+                end
+            end
+        end
+    end
+    
+    
+end
+
---
+++ main.m
@@ -0,0 +1,48 @@
+% DONE : Implement crossover func
+%       input  : takes 2 chromosome, crossover_ratio, chromosome_split
+%       output : returns new 2 chromosome
+% DONE : Implement mutation func
+%       input  : takes 1 chromosome, mutation_ratio
+%       output : returns new 1 chromosome
+% DONE : Implement elitism func
+%       input  : takes whole population, ratio
+%       output : returns subpopulation
+% DONE : Implement roulette func
+%       input  : takes whole population
+%       output : returns 2 chromosome
+
+chromosome_len   = 10;
+population_size  = 200;
+crossover_ratio  = 0.95;
+mutation_ratio   = 0.05;
+elitism_ratio = 0.02;
+chromosome_split = 6/chromosome_len;
+iteration_size = 200;
+
+tic
+
+genetic = Genetic_Algorithm(chromosome_len,population_size,crossover_ratio,mutation_ratio,elitism_ratio,chromosome_split,iteration_size);
+genetic.run();
+
+toc
+
+fit_hist = genetic.fitness_history;
+plot([1:length(fit_hist)],fit_hist, '.b')
+legend(strcat('fitness : ',num2str(fit_hist(length(fit_hist)))), 'Location', 'southeast')
+
+
+
+
+
+
+%     hold off
+%     [x,y] = meshgrid(-10:0.1:10,-10:0.1:10);
+%     %x = x - 4;
+%     %y = y + 3;
+%     z = -((x-4).^2 + (y+3).^2);
+%     mesh(x,y,z)
+%     hold on
+%     genetic.scatter()
+
+
+
