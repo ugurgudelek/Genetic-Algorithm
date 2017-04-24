50da08c7f0a9dff1d554f80b0b5883b29eb7e440
T1700\Doga <dogaceylan737@gmail.com>
2017-04-24 12:41:40 +0300

    to do list

---
+++ GA with Pressure Constraint/test_for_1mj.m
@@ -0,0 +1,51 @@
+    x1=2;
+    x2=3;
+    %x3=params(3);
+    x3=12.5-(x1+x2);
+    x4=10;
+    x5=10;
+    x6=10;
+    mass_projectile=0.125;
+    
+    rail_to_rail=2*(x1+x2+x3);
+
+        
+    FEA_Result = FEM_t_1mj_pressure_friction2(x1,x2,x3,x4,x5,x6,0);
+    
+   % 1)F_armature, 2)Pressure, 3)F_rail 4)Fcontact_from_armature_volume 5)Fcontact_from_armature_boundary 6) mass
+    mass_armature=FEA_Result(1,6);
+    Pressure=FEA_Result(1,2);
+    force_peak_armature=FEA_Result(1,1); %maximum force on armature
+
+    %import sampled current data
+    text = '1mJ.txt'; %import sampled data
+    textdata=importdata(text);
+    data_number=size(textdata.data,1); %find # of data
+    data_number=(round(data_number/2)-1)*2;
+    current=textdata.data(1:data_number,2); %save current and time array from the text file
+    time=textdata.data(1:data_number,1);
+    %time=time.*1e-6;
+    
+    peak_current=max(current); %find peak current to control L' from peak force (below)
+
+
+   
+    Lprime=2*force_peak_armature/(peak_current*peak_current);%L' should be approximately 0.5uH/m
+
+    current_square=(current).*(current); %Force is related with square of current
+    peak_current_square=max(current_square);
+    waveform=current_square/peak_current_square;%find normalized force and acceleration waveforms
+    %plot(time,waveform); %normalized waveform
+
+%     mass_projectile=0.125;
+    acc_projectile_peak=force_peak_armature/(mass_armature+mass_projectile); %maximum acceleration of projectile+armature
+    acc_waveform=acc_projectile_peak*waveform;%find actual acceleration waveform
+    %plot(time,acc_waveform)
+
+
+
+    velocity = trapz(time,acc_waveform(1:data_number));
+
+    energy=0.5*(mass_projectile)*velocity*velocity;
+
+       
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Genetic-Algorithm
+
+To Do List:
+
+- Şuan Pressure hesaplama constraintler arasında ve her kromozom için ayrıca pressure hesaplanıyor ve FEM çağırılıyor. Bunun yerine constraintler arasından pressure hesaplama kalkacak. Fittness function içinde zaten pressure hesaplanabiliyor. Pressure'ı önemsemeden tüm kromozomlar için tüm değerler hesaplanacak (değerlerden kastım FEM fonksiyonunun döndürdükleri) Daha sonra bu değerler içinde pressure da olduğundan pressure'ı 100MPa'dan büyük olanlar popilasyondan silinecek ve daha sonra kalanlar rulete sokulup elitlik mutasyon vs'ye uğrayacak.
+
+- Çözüm sırasında şuanda sadece Energy frafiği çiziliyor. Çözüm sırasında
+	- L'
+	- Mass_projectile
+	- Pressure
+	- Energy
+	- velocity
+
+grafikleri de çizilecek.
+
+- Historyde bu değerlerin sadece fittestları değil tüm değerlerinin bilgisi tutulacak makalede tüm dataların scatter figürlerini koyacaz.
