562d30f792d4d441da8a3aa73fae495564b5838f
Ugur Gudelek <ugurgudelek@gmail.com>
2017-10-19 17:34:00 +0300

    init commit

---
+++ Plasma Transaction/src/main.py
@@ -0,0 +1,17 @@
+from matlab_wrapper import MatlabWrapper
+
+matlab = MatlabWrapper()
+fem_output = matlab.fem_function([5,7,8,9])
+
+print(matlab.fem_function([5,7,8,9]))
+print(matlab.fem_function([5,7,8,9]))
+print(matlab.fem_function([5,7,8,9]))
+print(matlab.fem_function([5,7,8,9]))
+print(matlab.fem_function([5,7,8,9]))
+print(matlab.fem_function([5,7,8,9]))
+print(matlab.fem_function([5,7,8,9]))
+print(matlab.fem_function([5,7,8,9]))
+
+
+
+
